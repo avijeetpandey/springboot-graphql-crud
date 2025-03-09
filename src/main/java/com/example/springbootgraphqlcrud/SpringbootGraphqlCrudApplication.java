c681e97d240b8e8e866b12acc489a88d7651977c
Avijeet <avijeetpandey25@gmail.com>
2025-03-09 15:33:20 +0530

    add: graphql schema and types

---
+++ src/main/java/com/example/springbootgraphqlcrud/SpringbootGraphqlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgraphqlcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraphqlCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGraphqlCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootgraphqlcrud/SpringbootGraphqlCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootgraphqlcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGraphqlCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
